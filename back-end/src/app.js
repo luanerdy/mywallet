08d741f1598f698f24cbf9d016d0595a2fe6b993
ronaldjunor2 <ronaldjunior2@hotmail.com>
2021-07-01 15:25:05 -0300

    preparando review

--- back-end/src/app.js
+++
@@ -1,8 +0,0 @@
-import { customExpress } from './config/customExpress.js';
-import { routes } from './routes/routes.js';
-
-const port = process.env.PORT || 4000;
-const app = customExpress();
-routes(app);
-
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
